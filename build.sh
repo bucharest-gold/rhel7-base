b7086206cc8c81c813855f93dc8d10a787e5090f
Helio Frota <00hf11@gmail.com>
2018-10-31 08:53:37 -0300

    fix: docker-squash usage

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 
 echo "-> Building bucharestgold/rhel7-base ..."
 docker build --build-arg USER=${USER} --build-arg PASS=${PASS} -t bucharestgold/rhel7-base .
-docker-squash -f registry.access.redhat.com/rhel7 bucharestgold/rhel7-base:latest -t bucharestgold/rhel7-base:latest
+# docker-squash -f 2 registry.access.redhat.com/rhel7 bucharestgold/rhel7-base:latest -t bucharestgold/rhel7-base:latest
+docker-squash -f 3 -t bucharestgold/rhel7-base:squashed bucharestgold/rhel7-base:latest
 ./test.sh
